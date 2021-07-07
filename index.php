73aaea7051f2a6f60d0ce64ecb1a27e5189eda38
dlirA01 <arild.edvin.andreassen@gmail.com>
2021-07-08 01:10:17 +0200

    Added imports for items in head and default styling
    Added:
    - Imports for items in head
    - Default styling
    - Concept for profile page

--- index.php
+++ index.php
@@ -1,4 +1,58 @@
 <?php
-session_start();
-if (!isset($_SESSION["user"])) header("Location: login.php");
+$rootPath = "";
+?>
+<!DOCTYPE html>
+<html lang="no">
+<head>
+    <!-- Meta Tags -->
+    <?php require "structure/head/meta.php" ?>
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <meta name="author" content="">
+    <!-- ======= -->
+    <!-- General -->
+    <title>Lanmine</title>
+    <!-- ======= -->
+    <!-- Imports -->
+    <?php require "structure/head/imports.php" ?>
+</head>
+<body>
 
+<main id="main" class="flexbox-col">
+
+    <!-- Profile Page -->
+    <div class="view-width">
+
+        <!-- Profile Header -->
+        <div class="profile-header">
+            <div class="profile-header-inner flexbox">
+                <div class="phi-info-wrapper flexbox">
+                    <!-- Profile Picture -->
+                    <div class="phi-profile-picture-wrapper">
+                        <div class="phi-profile-picture-inner flexbox">
+                            <img class="phi-profile-picture" src="https://images.unsplash.com/photo-1532373213958-5156f1836b37?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1931&q=80" alt="">
+                        </div>
+                        <img class="phi-profile-picture-blur" src="https://images.unsplash.com/photo-1532373213958-5156f1836b37?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1931&q=80" alt="">
+                    </div>
+                    <!-- Profile Username -->
+                    <div class="phi-profile-username-wrapper">
+                        <h3 class="phi-profile-username">Username</h3>
+                    </div>
+                </div>
+                <!-- Profile Header Image -->
+                <div class="profile-header-overlay"></div>
+                <img class="profile-header-image" src="https://images.unsplash.com/photo-1612978935891-cd64257340c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1934&q=80" alt="">
+            </div>
+            <!-- Profile Header Image Blur -->
+            <img class="profile-header-image-blur" src="https://images.unsplash.com/photo-1612978935891-cd64257340c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1934&q=80" alt="">
+        </div>
+
+    </div>
+
+</main>
+
+</body>
+<script>
+
+</script>
+</html>
