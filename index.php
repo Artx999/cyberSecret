211ffadf68e3eb35db757648578252343f87296d
dlirA01 <arild.edvin.andreassen@gmail.com>
2021-07-08 02:32:06 +0200

    Update to profile header image and navbar
    Added:
    - Navbar
    
    Updated:
    - Profile Header

--- index.php
+++ index.php
@@ -5,51 +5,66 @@ $rootPath = "";
 <html lang="no">
 <head>
     <!-- Meta Tags -->
-    <?php require "structure/head/meta.php" ?>
+    <?php require "{$rootPath}structure/head/meta.php" ?>
     <meta name="description" content="">
     <meta name="keywords" content="">
     <meta name="author" content="">
     <!-- ======= -->
     <!-- General -->
-    <title>Lanmine</title>
+    <title>Cyber Secret</title>
     <!-- ======= -->
     <!-- Imports -->
-    <?php require "structure/head/imports.php" ?>
+    <?php require "{$rootPath}structure/head/imports.php" ?>
 </head>
 <body>
 
-<main id="main" class="flexbox-col">
+<!-- Page -->
+<div class="page">
 
-    <!-- Profile Page -->
-    <div class="view-width">
+    <!-- Navbar -->
+    <?php require "{$rootPath}structure/items/navbar.php" ?>
 
-        <!-- Profile Header -->
-        <div class="profile-header">
-            <div class="profile-header-inner flexbox">
-                <div class="phi-info-wrapper flexbox">
-                    <!-- Profile Picture -->
-                    <div class="phi-profile-picture-wrapper">
-                        <div class="phi-profile-picture-inner flexbox">
-                            <img class="phi-profile-picture" src="https://images.unsplash.com/photo-1532373213958-5156f1836b37?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1931&q=80" alt="">
+    <!-- Main -->
+    <main id="main" class="flexbox-col-start-center">
+
+        <!-- Profile Page -->
+        <div class="view-width">
+
+            <!-- Profile Header -->
+            <section class="profile-header">
+                <div class="profile-header-inner flexbox">
+                    <div class="phi-info-wrapper flexbox">
+                        <!-- Profile Picture -->
+                        <div class="phi-profile-picture-wrapper">
+                            <div class="phi-profile-picture-inner flexbox">
+                                <img class="phi-profile-picture" src="https://images.unsplash.com/photo-1532373213958-5156f1836b37?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1931&q=80" alt="">
+                            </div>
+                            <img class="phi-profile-picture-blur" src="https://images.unsplash.com/photo-1532373213958-5156f1836b37?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1931&q=80" alt="">
                         </div>
-                        <img class="phi-profile-picture-blur" src="https://images.unsplash.com/photo-1532373213958-5156f1836b37?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1931&q=80" alt="">
+                        <!-- Profile Username -->
+                        <div class="phi-profile-username-wrapper flexbox-col-left">
+                            <h3 class="phi-profile-username flexbox">Username <span class="material-icons-sharp">verified_user</span></h3>
+                            <p class="phi-profile-tagline">Something neat and funny!</p>
+                        </div>
                     </div>
-                    <!-- Profile Username -->
-                    <div class="phi-profile-username-wrapper">
-                        <h3 class="phi-profile-username">Username</h3>
-                    </div>
+                    <!-- Profile Header Image -->
+                    <div class="profile-header-overlay"></div>
+                    <img class="profile-header-image" src="https://images.unsplash.com/photo-1564737956548-df7b58e61146?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1950&q=80" alt="">
                 </div>
-                <!-- Profile Header Image -->
-                <div class="profile-header-overlay"></div>
-                <img class="profile-header-image" src="https://images.unsplash.com/photo-1612978935891-cd64257340c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1934&q=80" alt="">
-            </div>
-            <!-- Profile Header Image Blur -->
-            <img class="profile-header-image-blur" src="https://images.unsplash.com/photo-1612978935891-cd64257340c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1934&q=80" alt="">
+            </section>
+
+            <!-- Profile Page -->
+            <section class="profile-page">
+                <div class="profile-page-inner">
+                    <h3>Statistikk</h3>
+                </div>
+            </section>
+
         </div>
 
-    </div>
+    </main>
 
-</main>
+</div>
 
 </body>
 <script>
