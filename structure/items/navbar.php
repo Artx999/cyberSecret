7f7c6040564226681d2e296aa9be42a886f06239
dlirA01 <arild.edvin.andreassen@gmail.com>
2021-07-08 02:37:25 +0200

    Removed the entire navbar
    removed the entire navbar because i'm indecisive as f*ck and also did some weird thing to the profile picture that will probably not stay

--- structure/items/navbar.php
+++ structure/items/navbar.php
@@ -1,38 +1,3 @@
 <nav id="navbar">
-    <div class="navbar-inner flexbox-col-start-center">
-        <!-- Navbar Profile Picture -->
-        <div class="navbar-profile-picture-wrapper">
-            <div class="npp-inner">
-                <img class="npp" src="https://images.unsplash.com/photo-1532373213958-5156f1836b37?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1931&q=80" alt="">
-            </div>
-            <img class="npp-blur" src="https://images.unsplash.com/photo-1532373213958-5156f1836b37?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1931&q=80" alt="">
-        </div>
-        <!-- Navbar Items -->
-        <ul class="navbar-items flexbox-col">
-            <!-- Items -->
-            <li class="navbar-item flexbox">
-                <a class="navbar-item-inner flexbox" href="index.php">
-                    <span class="material-icons-sharp">groups</span>
-                </a>
-            </li>
-            <li class="navbar-item flexbox">
-                <a class="navbar-item-inner flexbox" href="index.php">
-                    <span class="material-icons-sharp">auto_stories</span>
-                </a>
-            </li>
-            <li class="navbar-item flexbox">
-                <a class="navbar-item-inner flexbox" href="index.php">
-                    <span class="material-icons-sharp">settings</span>
-                </a>
-            </li>
-        </ul>
-        <!-- Navbar Statistics -->
-        <div class="navbar-stats">
-            <ul class="nst-items">
-                <li class="nst-item flexbox">
-                    <a class="nst-item-inner flexbox"><span class="material-icons-sharp">create</span></a>
-                </li>
-            </ul>
-        </div>
-    </div>
+
 </nav>
