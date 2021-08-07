59a31edfa061cfe3ec9611a535dbfb09ba32fd1f
Artx999 <54852665+Artx999@users.noreply.github.com>
2021-08-07 23:58:48 +0200

    Improved DB according to req spec
    - Updated user by adding seat number and removing email and birthday.
    - Changed the stats.
    - Added inventory.

--- backend/db/cybersecret.sql
+++ backend/db/cybersecret.sql
@@ -16,6 +16,30 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `inventory`
+--
+
+DROP TABLE IF EXISTS `inventory`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `inventory` (
+  `user_id` int(11) NOT NULL,
+  `item` varchar(255) NOT NULL,
+  KEY `inventory_user_user_id_fk` (`user_id`),
+  CONSTRAINT `inventory_user_user_id_fk` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `inventory`
+--
+
+LOCK TABLES `inventory` WRITE;
+/*!40000 ALTER TABLE `inventory` DISABLE KEYS */;
+/*!40000 ALTER TABLE `inventory` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `stats`
 --
 
@@ -24,10 +48,12 @@ DROP TABLE IF EXISTS `stats`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `stats` (
   `user_id` int(11) NOT NULL,
-  `strength` int(11) NOT NULL DEFAULT 0,
-  `dexterity` int(11) NOT NULL DEFAULT 0,
-  `charisma` int(11) NOT NULL DEFAULT 0,
-  `intelligence` int(11) NOT NULL DEFAULT 0,
+  `strength` int(11) NOT NULL DEFAULT 5,
+  `dexterity` int(11) NOT NULL DEFAULT 5,
+  `intelligence` int(11) NOT NULL DEFAULT 5,
+  `wisdom` int(11) NOT NULL DEFAULT 5,
+  `charisma` int(11) NOT NULL DEFAULT 5,
+  `luck` int(11) NOT NULL DEFAULT 5,
   PRIMARY KEY (`user_id`),
   CONSTRAINT `stats_user_user_id_fk` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -39,6 +65,7 @@ CREATE TABLE `stats` (
 
 LOCK TABLES `stats` WRITE;
 /*!40000 ALTER TABLE `stats` DISABLE KEYS */;
+INSERT INTO `stats` VALUES (1,7,5,9,10,5,8),(21,5,7,5,5,5,5);
 /*!40000 ALTER TABLE `stats` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -52,14 +79,12 @@ DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
   `user_id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(25) NOT NULL,
-  `email` varchar(255) DEFAULT NULL,
   `password` varchar(255) NOT NULL,
-  `birthday` date DEFAULT NULL,
+  `seat_number` int(11) NOT NULL,
   `timestamp` datetime NOT NULL DEFAULT current_timestamp(),
   PRIMARY KEY (`user_id`),
   UNIQUE KEY `user_username_uindex` (`username`),
-  UNIQUE KEY `user_user_id_uindex` (`user_id`),
-  UNIQUE KEY `user_email_uindex` (`email`)
+  UNIQUE KEY `user_seat_number_uindex` (`seat_number`)
 ) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -69,7 +94,7 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (1,'Artx999','mrjj@live.no','$2y$10$9pVEbwFmFzCjYt91kRghdOyNX1pkPabf2GyhLBUcffFPC2mexsuAG','2002-11-27','2021-01-17 00:23:58'),(7,'iohj','uiohl','$2y$10$uiigoQ6fV3/xOEGYqnCfbuisdDdHpHGqjoo8WuQ2gD7QI9XtIZkjW','1200-12-12','2021-01-18 21:10:18'),(8,'a','a','$2y$10$VvOS4ZClLlq01SxHK6LUpu8x3vId9wRKKwDEEqPF4A7d6XW5TWWYG',NULL,'2021-01-26 12:18:52'),(10,'xfvgynj cghbvujm ','dfhrtgfdxrthgb','$2y$10$PamA4QkEidoYvTa3vLHMcevVn7UEgJGEDHE6yLZP89dxy3377QEjm','0000-00-00','2021-07-08 01:24:42'),(11,'gzdb rtxe','hbaedrtg','$2y$10$DbZHaRq93waW02ov4AaO1.H53yNoOO6f49BSj88YHKSZayibIe2T2','0000-00-00','2021-07-08 01:25:48'),(12,'yutrhea','kjtrhewr','$2y$10$NynlWmJ8nMzKn0IVJFPQmufni1tyKd9bUiizK2crWTOo5A4adDF0m','0000-00-00','2021-07-08 01:27:05'),(13,'hdaet','grw','$2y$10$jIVwBuSVYn35ejAJPH/gOOAkfoUSMf6OStFwhFsnvoOpLxP3oqhJO','0000-00-00','2021-07-08 01:31:23'),(16,'awdrfafqawf','qeqwrwqrf','$2y$10$UPvugv4/Uro52HWFLK2LJ.J6W6/wfHnHBhHDeMVneCDeh3GWGefWO','0000-00-00','2021-07-08 01:38:23'),(20,'liukgycfjghxyhdtxzgdrz','fxgnhnfghcghmmghcjmghjc','$2y$10$IIOOhzE/CrAuCzt/cWwkYON1sDzoipK39.Y77Sfl8fSTA69529cSu','0000-00-00','2021-07-08 01:58:56'),(21,'lol','lol','$2y$10$Z0rmkYzmGFQLY3WuVdcGsOo9MYFxhESb7TOs8DhanGLD7HLEp/rDa','0000-00-00','2021-07-08 02:06:35'),(22,'lkjhgfd','fdsghjfgk','$2y$10$wXRhQA0nMW/sck4FuBa.de67l6C48epOezv26R1t5cyOquOPDw7cC','0000-00-00','2021-07-08 03:28:30'),(23,'ojoi',NULL,'$2y$10$OCDiSFDl0khpfWV7hkh65.20hCqPfAndUAUyyJEXknxj77RTEyPY6','0000-00-00','2021-07-13 23:59:55'),(24,'tygh',NULL,'$2y$10$KVR7mJbjH9JnOiJba3xBn.BfVun8jh16IzuD4OMbInzHlov633l0G',NULL,'2021-07-14 00:24:19');
+INSERT INTO `user` VALUES (1,'Artx999','$2y$10$9pVEbwFmFzCjYt91kRghdOyNX1pkPabf2GyhLBUcffFPC2mexsuAG',9999,'2021-01-17 00:23:58'),(21,'lol','$2y$10$Z0rmkYzmGFQLY3WuVdcGsOo9MYFxhESb7TOs8DhanGLD7HLEp/rDa',9998,'2021-07-08 02:06:35'),(22,'Kåre Gunnar','$2y$10$wXRhQA0nMW/sck4FuBa.de67l6C48epOezv26R1t5cyOquOPDw7cC',9997,'2021-07-08 03:28:30'),(23,'Bonny','$2y$10$OCDiSFDl0khpfWV7hkh65.20hCqPfAndUAUyyJEXknxj77RTEyPY6',9996,'2021-07-13 23:59:55'),(24,'Kyle Ronny','$2y$10$KVR7mJbjH9JnOiJba3xBn.BfVun8jh16IzuD4OMbInzHlov633l0G',9995,'2021-07-14 00:24:19');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -82,4 +107,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-07-14  0:38:27
+-- Dump completed on 2021-08-07 23:56:46
