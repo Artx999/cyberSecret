fe86c628c686d73ffe665a70d6bd3191d71551dc
dlirA01 <arild.edvin.andreassen@gmail.com>
2021-09-08 00:55:13 +0200

    don't mind this...

--- signup.php
+++ signup.php
@@ -32,16 +32,16 @@ $rootPath = "";
             <h2>Registrer deg</h2>
             <a href="login.php">Eller login</a>
         </div>
-        <p class="error-msg">
-            <?php
 
-            require "func.php";
-            if (isset($_GET["error"])) {
-                print '<p class="error-msg">' . ErrorMsg::decode($_GET["error"]) . '</p>'; // Prints error messages
-            }
+        <?php
 
-            ?>
-        </p>
+        require "func.php";
+        if (isset($_GET["error"])) {
+            print '<p class="error-msg">' . ErrorMsg::decode($_GET["error"]) . '</p>'; // Prints error messages
+        }
+
+        ?>
+
         <div class="user-form-center">
             <fieldset class="input-grid inpt-grd-2">
                 <div class="input-wrapper ufi-input-wrapper">
