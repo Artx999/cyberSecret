0bab6a9fcf55099180f834cdb3539c7e14cd18ff
Mathias René Juelsen-Jacobsen <54852665+Artx999@users.noreply.github.com>
2021-09-24 00:17:02 +0200

    Update sql-imort.bat

--- backend/db/sql-imort.bat
+++ backend/db/sql-imort.bat
@@ -1,3 +1,3 @@
-C:\xampp\mysql\bin\mysql -u root cyber_secret < C:\xampp\htdocs\cyberSecret\backend\db\cybersecret.sql
+C:\xampp\mysql\bin\mysql -u root lanmine_noneon < C:\xampp\htdocs\cyberSecret\backend\db\db.sql
 @ECHO Finished importing DB
 @PAUSE
