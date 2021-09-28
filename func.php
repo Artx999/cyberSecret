4e8b9ca10bb0df1a4e3f7610127469ca22ead033
Mathias René Juelsen-Jacobsen <54852665+Artx999@users.noreply.github.com>
2021-09-28 18:46:01 +0200

    Added comments to func.php
    Also prepared test.php

--- func.php
+++ func.php
@@ -1,9 +1,11 @@
 <?php
-// ----------------
+// Simple function that checks if a condition is true or false
 function trueOrFalse($i) {
     if ($i) return "true";
     else return "false";
 }
+
+// Function that queries the database and returns the result, formatted. All database login credentials can be changed
 // TODO: Possibly make this a class that has functionality described in: https://stackoverflow.com/questions/533459/how-to-do-a-php-nested-class-or-nested-methods
 function dbQuery($sql, $database = "lanmine_noneon", $server = "localhost", $username="root", $password="") {
     $connection = new mysqli($server, $username, $password, $database);
@@ -15,21 +17,25 @@ function dbQuery($sql, $database = "lanmine_noneon", $server = "localhost", $use
     }
 }
 
-// ----------------
+
+// Class for storing and handling errors, as well as printing them
 class ErrorMsg {
     public $content;
     public function __construct() {
         $this->content = [];
     }
 
+    // Function for adding errors to the object
     public function add($message) {
         array_push($this->content, $message);
     }
 
+    // Function that encodes the error object to base64
     public function encode() {
         return base64_encode(json_encode($this->content));
     }
 
+    // Static function that decodes error message object, and returns all the errors in a readable and formatted way
     public static function decode($error) {
         $error = json_decode(base64_decode($error));
         $errorList = fopen("errors.json", "r");
@@ -44,8 +50,7 @@ class ErrorMsg {
 }
 
 
-
-
+// Class for users logged in
 class User {
     public $userId, $username, $cardID, $profilePicture;
     function __construct($userId, $username, $cardID, $profilePicture) {
@@ -57,14 +62,17 @@ class User {
         } else $this->profilePicture = file_get_contents("images/profile-pic.jpg");
     }
 
+    // Function that adds user to session (should be used with user object)
     public function sessionSet() {
         $_SESSION["user"] = serialize($this);
     }
 
+    // Function that gets the current user from session (should be used with user object)
     public static function sessionGet() {
         return unserialize($_SESSION["user"]);
     }
 
+    // Static function that authenticates user and returns user object
     public static function auth($uname, $password) {
         $hashedPass = User::getHashedPass($uname);
         if (password_verify($password, $hashedPass)) {
@@ -73,18 +81,21 @@ class User {
         } else return false;
     }
 
+    // Private static function that returns hashed password from username
     private static function getHashedPass($uname) {
         $result = dbQuery("SELECT username, password FROM lanmine_noneon.user WHERE username = '$uname' LIMIT 1;");
         if ($result = $result->fetch_assoc()) return $result["password"];
         else return false;
     }
 
+    // Function that returns user stats as stat object
     public function getStats() {
         $i = dbQuery("SELECT * FROM lanmine_noneon.stats WHERE user_id='$this->userId'")->fetch_assoc();
         if ($i)return new Stats($i["strength"], $i["dexterity"], $i["intelligence"], $i["wisdom"], $i["charisma"], $i["luck"]);
         else return new Stats(5, 5, 5, 5, 5, 5);
     }
 
+    // Function that returns users inventory
     public function getInventory() {
         $i = dbQuery("SELECT * FROM lanmine_noneon.inventory WHERE user_id='$this->userId'");
         if ($i) return $i;
@@ -92,6 +103,8 @@ class User {
     }
 }
 
+
+// Class for storing stats
 class Stats {
     public $strength, $dexterity, $intelligence, $wisdom, $charisma, $luck;
     public function __construct($strength, $dexterity, $intelligence, $wisdom, $charisma, $luck) {
@@ -104,8 +117,10 @@ class Stats {
     }
 }
 
+
+// Class for variable info for the LAN
 class Info {
     public static function maxID() {
-        return 172; // ! Must ask someone important !
+        return 172;
     }
 }
--- test.php
+++ test.php
@@ -1,15 +1,6 @@
 <?php
 require "func.php";
 session_start();
+$errors = new ErrorMsg();
+$rootPath = "";
 
-
-$user = User::auth("Artx999", "test");
-$inventory = $user->getInventory();
-if ($inventory->fetch_assoc()) {
-    foreach ($inventory as $row) {
-        print_r($row["item"]);
-        print "</br>";
-    }
-} else {
-    print "NO!";
-}
