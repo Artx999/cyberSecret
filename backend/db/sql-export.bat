207ed06ddf916a39728ab3396edc6a56ce64ceff
Mathias René Juelsen-Jacobsen <54852665+Artx999@users.noreply.github.com>
2021-09-30 20:48:07 +0200

    Removed db folder

--- backend/db/sql-export.bat
+++
@@ -1,3 +0,0 @@
-C:\xampp\mysql\bin\mysqldump -u root lanmine_noneon > C:\xampp\htdocs\cyberSecret\backend\db\db.sql
-@ECHO Finished exporting DB
-@PAUSE
--- backend/db/sql-imort.bat
+++
@@ -1,3 +0,0 @@
-C:\xampp\mysql\bin\mysql -u root lanmine_noneon < C:\xampp\htdocs\cyberSecret\backend\db\db.sql
-@ECHO Finished importing DB
-@PAUSE
