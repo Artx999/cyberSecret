296b024ef06a3c0099643756f5060b985a980dfe
Mathias René Juelsen-Jacobsen <54852665+Artx999@users.noreply.github.com>
2021-10-07 05:06:30 +0200

    Added favicon

--- structure/head/imports.php
+++ structure/head/imports.php
@@ -5,6 +5,8 @@
 <link rel="apple-touch-icon" sizes="180x180" href="<?php print $rootPath ?>images/favicon/apple-touch-icon.png">
 <link rel="shortcut icon" href="<?php print $rootPath ?>images/favicon/favicon.ico" type="image/x-icon">
 -->
+<!-- Favicon -->
+<link rel="icon" type="image/png" href="<?php print $rootPath ?>images/eslglogo_transparrent.png">
 <!-- Fonts -->
 <link rel="preconnect" href="https://fonts.gstatic.com">
 <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
